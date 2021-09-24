1d1fc37e79308ba558e1a7ed7307e9d41780dbe2
root <root@alpha>
2021-09-24 17:54:52 +0000

    update: change filename

--- global/common.mk
+++ global/common.mk
@@ -4,8 +4,8 @@ CC ?= gcc
 
 OBJECT_LIBBPF = $(LIBBPF_DIR)/libbpf.a
 
-USER_C = ${USER_TARGET:=.c}
-USER_OBJ = ${USER_C:.c=.o}
+USER_C := ${USER_TARGET:=.c}
+USER_OBJ := ${USER_C:.c=.o}
 USER_CFLAGS ?= -I$(LIBBPF_DIR)/build/usr/include/ -g
 LDFLAGS ?= -L$(LIBBPF_DIR)
 
@@ -15,7 +15,7 @@ BPF_CFLAGS ?= -I$(LIBBPF_DIR)/build/usr/include/
 
 LIBS = -l:libbpf.a -lelf -lz $(USER_LIBS)
 
-OUTPUT_DIR ?= ./build
+OUTPUT_DIR ?= ./
 
 all: llvm-check $(USER_TARGET) $(XDP_OBJ)
 	@echo "alpha"
@@ -34,8 +34,7 @@ clean:
 	rm -rf $(LIBBPF_DIR)/build
 	$(MAKE) -C $(LIBBPF_DIR) clean
 	$(MAKE) -C $(COMMON_DIR) clean
-	rm -rf $(OUTPUT_DIR)
-	rm -f $(XDP_OBJ) 
+	rm -f $(XDP_OBJ) $(USER_TARGET)
 	rm -f *.ll
 	rm -f *~
 
@@ -43,7 +42,7 @@ COMMON_MK = $(COMMON_DIR)/common.mk
 
 COMMON_OBJS += $(COMMON_DIR)/cmd_args.o $(COMMON_DIR)/xdp_helper.o
 $(COMMON_OBJS):
-	make -C $(COMMON_DIR) ${notdir $@}
+	make -C $(COMMON_DIR)
 
 $(OBJECT_LIBBPF):
 	@if [ ! -d $(LIBBPF_DIR) ]; then \
@@ -57,7 +56,7 @@ $(OBJECT_LIBBPF):
 
 $(USER_TARGET): %: %.c $(OBJECT_LIBBPF) Makefile $(COMMON_MK) $(COMMON_OBJS)
 	mkdir -p $(OUTPUT_DIR)
-	$(CC) -Wall $(USER_CFLAGS) $(LDFLAGS) -o $(OUTPUT_DIR)/$@ $< $(COMMON_OBJS) $(LIBS)
+	$(CC) -Wall $(USER_CFLAGS) $(LDFLAGS) -o $(OUTPUT_DIR)/$@ $(COMMON_OBJS) $< $(LIBS)
 
 $(XDP_OBJ): %.o: %.c $(OBJECT_LIBBPF) Makefile $(COMMON_MK)
 	mkdir -p $(OUTPUT_DIR)
